5a26e40c90d94ca828acde987c00b9340f7e758d
kachiDeGreat <mail.ricx@gmail.com>
2025-07-09 17:53:26 +0100

    ceated project folder and navbar

---
+++ src/app/ui/pages/home/Home.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+
+function Home() {
+  return (
+    <>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum
+        accusantium saepe laboriosam! Ullam cum incidunt quisquam quaerat
+        officiis at, eos dolorum doloribus odit itaque fuga magni maxime fugit,
+        porro et cumque nulla iusto optio vitae ipsam facere consequatur
+        voluptates nihil!
+      </div>
+    </>
+  );
+}
+
+export default Home;
